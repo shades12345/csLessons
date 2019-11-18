0fcc518cdcd0c65dfd3e0f44d8b320dc75321f40
shades12345 <say4robert@gmail.com>
2019-11-18 12:29:56 +0200

    input speed and time

--- totalDistanta.py
+++ totalDistanta.py
@@ -1,11 +1,10 @@
 
 
 
-def dis(s):
-    distance = s*1
-      if distance >= 400:
-          print ("you traveled 400 or more km, go to sleep")
-      else:
-          print("you traveled only", distance, "DRIVE WILD!!")
-          
-          
+>>> def dis(s, t):
+...     distance = s*t
+...     if distance >= 400:
+...             print ("you traveled 400 or more km, go to sleep")
+...     else:
+...             print ("you traveled only", deistance, "DRIVE WILD!!")
+... 
