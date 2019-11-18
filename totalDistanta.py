0e2521702be524bb2c7a25375df5d509519c2d8f
shades12345 <say4robert@gmail.com>
2019-11-18 11:37:48 +0200

    input distance

---
+++ totalDistanta.py
@@ -0,0 +1,13 @@
+
+
+
+
+>>> def dis(d):
+...     min = d
+...     h = mis // 60
+...     if s >= 400:
+...             print("ia o pauza")
+...     else:
+...             print("with a speed of 60 km/h you will be driving for", h, "hours and", min, "minutes")
+... 
+>>> 
