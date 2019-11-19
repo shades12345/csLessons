315dfeedaf61ed4b686f98f402fdfbc4ddedf1ce
shades12345 <say4robert@gmail.com>
2019-11-19 12:50:25 +0200

    Update totalDistanta.py

--- totalDistanta.py
+++ totalDistanta.py
@@ -9,6 +9,7 @@
 ...     while d < 400:
 ...             print ("in", h, "hour(s), you traveled", d, "km")
 ...             t += 1
+...             d = s*t
 ...     if d >= 400:
 ...             print ("you traveled 400 or more km, go to sleep")
 ...
